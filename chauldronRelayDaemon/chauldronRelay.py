b837fdb74f5bbae9b378e8cb2f56cb62520ab047
jipandres <jipandres@gmail.com>
2017-02-01 15:57:14 +0100

    Update chauldronRelay.py
    appended an stupid example of an script

--- chauldronRelayDaemon/chauldronRelay.py
+++ chauldronRelayDaemon/chauldronRelay.py
@@ -27,3 +27,27 @@ GPIO.cleanup( [channel1, channel2] )
 
 #to change the GPIO value after pression a button.
 GPIO.output(12, not GPIO.input(12))
+
+
+
+###############################Another example
+
+#EJEMPLO DE BLINKING CON RASPBERRY PI
+#Escrito por Gl4r3
+import RPi.GPIO as GPIO #importamos la libreria y cambiamos su nombre por "GPIO"
+import time #necesario para los delays
+ 
+#establecemos el sistema de numeracion que queramos, en mi caso BCM
+GPIO.setmode(GPIO.BCM)
+ 
+#configuramos el pin GPIO17 como una salida
+GPIO.setup(17, GPIO.OUT)
+ 
+#encendemos y apagamos el led 5 veces
+for i in range(0,5):
+    GPIO.output(17, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(17, GPIO.LOW)
+    time.sleep(1)
+ 
+GPIO.cleanup()  #devuelve los pines a su estado inicial
