619d5dad4cef0efdb3fdfe3b9ead36e35ae7b2db
jipandres <jipandres@gmail.com>
2017-02-01 15:55:57 +0100

    Create installation.sh
    Including how to install the GPIO

---
+++ installation/installation.sh
@@ -0,0 +1,9 @@
+
+
+
+# install GPIO
+wget https://pypi.python.org/packages/source/R/RPi.GPIO/RPi.GPIO-0.5.4.tar.gz  <-- check version
+tar zxf RPi.GPIO-0.5.4.tar.gz
+cd RPi.GPIO-0.5.4.tar.gz
+sudo python setup.py install
+
