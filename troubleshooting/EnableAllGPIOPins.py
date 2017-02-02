dba5a24ae9b3e759eec5b4117d6f4a859bea2990
jipandres <jipandres@gmail.com>
2017-02-02 07:28:49 +0100

    Update EnableAllGPIOPins.py

--- troubleshooting/EnableAllGPIOPins.py
+++ troubleshooting/EnableAllGPIOPins.py
@@ -7,8 +7,11 @@ GPIO.setmode(GPIO.BCM)
 print "Start enabling GPIO pins with HIGH output value (for Model A & B -26pins) "
 for i in (2,3,4,7,8,9,10,11,14,15,17,18,22,23,24,25,27):
   print "enabling %d" %i
-  GPIO.setup(i, GPIO.OUT)
-  GPIO.output(i, GPIO.HIGH)
+  try:
+   GPIO.setup(i, GPIO.OUT)
+   GPIO.output(i, GPIO.HIGH)
+  except:
+   print "something failed with PIN %d" %i
   time.sleep(1)
 
 print "Finish task. Restoring"
