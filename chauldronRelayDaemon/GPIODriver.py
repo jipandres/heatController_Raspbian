61e3b8190103dfed59676692fe9e1c2b7116715e
jipandres <jipandres@gmail.com>
2017-02-03 10:35:36 +0100

    Update GPIODriver.py

--- chauldronRelayDaemon/GPIODriver.py
+++ chauldronRelayDaemon/GPIODriver.py
@@ -1,4 +1,7 @@
-import RPi.GPIO as GPIO
+try:
+  import RPi.GPIO as GPIO
+except ImportError:
+    raise ImportError('GPIO not available, please check installation procedure')
 
 
 class GPIODriver(ChauldronRelayIf):
